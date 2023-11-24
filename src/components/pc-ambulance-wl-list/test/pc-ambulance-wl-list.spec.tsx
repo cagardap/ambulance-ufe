8e9d1c1cb34b0197f5ebfe6b1ada573a3faf4c72
Peter Cagarda <peter.cagarda@siemens-healthineers.com>
2023-11-24 14:51:21 +0100

    unit test

--- src/components/pc-ambulance-wl-list/test/pc-ambulance-wl-list.spec.tsx
+++ src/components/pc-ambulance-wl-list/test/pc-ambulance-wl-list.spec.tsx
@@ -7,12 +7,11 @@ describe('pc-ambulance-wl-list', () => {
       components: [PcAmbulanceWlList],
       html: `<pc-ambulance-wl-list></pc-ambulance-wl-list>`,
     });
-    expect(page.root).toEqualHtml(`
-      <pc-ambulance-wl-list>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </pc-ambulance-wl-list>
-    `);
+
+    const wlList = page.rootInstance as PcAmbulanceWlList;
+    const expectedPatients = wlList?.waitingPatients?.length
+
+    const items = page.root.shadowRoot.querySelectorAll("md-list-item");
+    expect(items.length).toEqual(expectedPatients);
   });
 });
